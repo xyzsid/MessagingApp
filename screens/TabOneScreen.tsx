eb881b0c03ae124ce3719c87adea7707ede9dbea
xyzsid <sid.dommeti@gmail.com>
2021-09-15 12:29:38 -0500

    Refactoring

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,31 +1,30 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import { StyleSheet, View } from 'react-native';
+import ChatRoomItem from '../components/ChatRoomItem';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      
+    <View style={styles.page}>
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
+      <ChatRoomItem />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  page: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    backgroundColor: 'white',
   },
 });
